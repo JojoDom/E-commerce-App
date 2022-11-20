5280ff3fefb6d3ce582005ea2d340bb4da41fcfa
JojoDom <adwoa_dadson@yahoo.com>
2022-11-20 19:29:13 +0000

    added dark theme and light theme

---
+++ groceries/lib/utilities/helper_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Widget addVerticalSpace(double hieght) {
+  return SizedBox(
+    height: hieght,
+  );
+}
+
+Widget addHorizontalSpace(double width) {
+  return SizedBox(
+   width: width,
+  );
+}
