2276e9e3fd4f2c04da1d684f753d7833e0b880d7
JojoDom <adwoa_dadson@yahoo.com>
2023-02-21 12:01:12 +0000

    code cleanup

--- groceries/lib/screens/all_fruits_vegies.dart
+++ groceries/lib/screens/all_fruits_vegies.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 
 import '../models/fruits_and_vegetables_data.dart';
