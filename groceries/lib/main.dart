0eebdad805d9c3475124a085cf67dcbef4f8d7b0
JojoDom <adwoa_dadson@yahoo.com>
2023-05-08 06:31:16 +0000

    firebase massaging

--- groceries/lib/main.dart
+++ groceries/lib/main.dart
@@ -1,17 +1,30 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:groceries/dashboard/dashboard.dart';
-import 'package:groceries/screens/home/home.dart';
 import 'package:groceries/theme/themes.dart';
 import 'package:groceries/theme/themes_controller.dart';
+import 'package:logger/logger.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
+  final fcmToken = await FirebaseMessaging.instance;
+  FirebaseMessaging.instance.onTokenRefresh.listen((fcmToken) {
+    logger.i('REFRESH' + fcmToken);
+  }).onError((err) {
+  });
+  fcmToken.getToken().then(
+    (value) {
+      logger.i(fcmToken);
+    },
+  );
   runApp(const MyApp());
 }
 
 final themeController = Get.put(ThemeController());
+var logger = Logger();
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
