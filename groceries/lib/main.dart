f613074545cd438203e8173dc6d276e5daae6dbf
JojoDom <adwoa_dadson@yahoo.com>
2023-05-09 08:46:52 +0000

    firebase project set up

--- groceries/lib/main.dart
+++ groceries/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -9,15 +10,16 @@ import 'package:logger/logger.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await GetStorage.init();
-  final fcmToken = await FirebaseMessaging.instance;
+  final fcmToken = FirebaseMessaging.instance;
+  await FirebaseMessaging.instance.getInitialMessage();
   FirebaseMessaging.instance.onTokenRefresh.listen((fcmToken) {
-    logger.i('REFRESH' + fcmToken);
-  }).onError((err) {
-  });
+    logger.i('REFRESH ' + fcmToken);
+  }).onError((err) {});
   fcmToken.getToken().then(
     (value) {
-      logger.i(fcmToken);
+      logger.i('TOKEN ' + value!);
     },
   );
   runApp(const MyApp());
@@ -28,6 +30,7 @@ var logger = Logger();
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
