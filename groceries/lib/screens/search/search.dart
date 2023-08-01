8c55f6b475302dbb95143ca1423f4a457ec82d46
JojoDom <adwoa_dadson@yahoo.com>
2023-08-01 14:37:24 +0000

    add to list

--- groceries/lib/screens/search/search.dart
+++ groceries/lib/screens/search/search.dart
@@ -17,21 +17,63 @@ class _SearchState extends State<Search> {
         appBar: AppBar(
           elevation: 0,
           title: Text('Fruits'),
+          actions: [
+            InkWell(
+              onTap: () {
+                fruits.add(veggies[0]);
+              },
+              child: Container(
+                height: 30,
+                width: 30,
+                decoration: const BoxDecoration(
+                    shape: BoxShape.circle, color: Colors.green),
+                child: const Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            InkWell(
+              onTap: () {},
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 30,
+                  width: 30,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.red),
+                  child: const Icon(
+                    Icons.remove,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
         body: Padding(
           padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          child: ListView.builder(
-            itemCount: veggies.length,
+          child: ListView.separated(
+            itemCount: fruits.length,
             itemBuilder: ((context, index) {
-              return 
-              ListTile(title: Text(veggies[index].title),
-              leading: Container(
-                height: 50,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(5)),
-                child: Image.asset(veggies[index].image),),
-        
+              return ListTile(
+                title: Text(fruits[index].title),
+                leading: Container(
+                  height: 50,
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(5)),
+                  child: Image.asset(fruits[index].image),
+                ),
               );
             }),
+            separatorBuilder: (BuildContext context, int index) {
+              return Divider(
+                color: Colors.grey,
+              );
+            },
           ),
         ));
   }
