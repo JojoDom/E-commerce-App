62af9f607912d816311376b0cff528dca1f98f0b
JojoDom <adwoa_dadson@yahoo.com>
2023-08-08 12:05:23 +0000

    animated list

--- groceries/lib/screens/search/search.dart
+++ groceries/lib/screens/search/search.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../../models/food_model.dart';
-import '../../widgets/food_item_widget.dart';
 
 class Search extends StatefulWidget {
   const Search({Key? key}) : super(key: key);
@@ -12,75 +11,178 @@ class Search extends StatefulWidget {
 
 class _SearchState extends State<Search> {
   int i = 0;
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
+  late ListModel<FoodsData> _list;
+  FoodsData? _selectedItem;
+  late FoodsData _nextItem;
+
+  @override
+  void initState() {
+    super.initState();
+    _list = ListModel<FoodsData>(
+      listKey: _listKey,
+      initialItems: fruits,
+      removedItemBuilder: _buildRemovedItem,
+    );
+    _nextItem = veggies[i];
+  }
+
+  Widget _buildItem(
+      BuildContext context, int index, Animation<double> animation) {
+    return CardItem(
+      animation: animation,
+      item: _list[index],
+      selected: _selectedItem == _list[index],
+      onTap: () {
+        setState(() {
+          _selectedItem = _selectedItem == _list[index] ? null : _list[index];
+        });
+      },
+    );
+  }
+
+  Widget _buildRemovedItem(
+      FoodsData item, BuildContext context, Animation<double> animation) {
+    return CardItem(
+      animation: animation,
+      item: item,
+      // No gesture detector here: we don't want removed items to be interactive.
+    );
+  }
+
+  void _insert() {
+    final int index =
+        _selectedItem == null ? _list.length : _list.indexOf(_selectedItem!);
+    _list.insert(index, veggies[i++]);
+  }
+
+  void _remove() {
+    if (_selectedItem != null) {
+      _list.removeAt(_list.indexOf(_selectedItem!));
+      setState(() {
+        _selectedItem = null;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+      home: Scaffold(
         appBar: AppBar(
+          title: const Text('Fruits and Veggies'),
           elevation: 0,
-          title: Text('Fruits'),
-          actions: [
-            InkWell(
-              onTap: () {
-                fruits.insert(0, veggies[i++]);
-                setState(() {});
-              },
-              child: Container(
-                height: 30,
-                width: 30,
-                decoration: const BoxDecoration(
-                    shape: BoxShape.circle, color: Colors.green),
-                child: const Icon(
-                  Icons.add,
-                  color: Colors.white,
-                ),
-              ),
+          backgroundColor: Colors.green,
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.add_circle),
+              onPressed: _insert,
+              tooltip: 'insert a new item',
             ),
-            const SizedBox(
-              width: 10,
+            IconButton(
+              icon: const Icon(Icons.remove_circle),
+              onPressed: _remove,
+              tooltip: 'remove the selected item',
             ),
           ],
         ),
         body: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          child: ListView.separated(
-            itemCount: fruits.length,
-            itemBuilder: ((context, index) {
-              return ListTile(
-                title: Text(fruits[index].title),
+          padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 20),
+          child: AnimatedList(
+            key: _listKey,
+            initialItemCount: _list.length,
+            itemBuilder: _buildItem,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ListModel<E> {
+  ListModel({
+    required this.listKey,
+    required this.removedItemBuilder,
+    Iterable<E>? initialItems,
+  }) : _items = List<E>.from(initialItems ?? <E>[]);
+
+  final GlobalKey<AnimatedListState> listKey;
+  final RemovedItemBuilder<E> removedItemBuilder;
+  final List<E> _items;
+
+  AnimatedListState? get _animatedList => listKey.currentState;
+
+  void insert(int index, E item) {
+    _items.insert(index, item);
+    _animatedList!.insertItem(index);
+  }
+
+  E removeAt(int index) {
+    final E removedItem = _items.removeAt(index);
+    if (removedItem != null) {
+      _animatedList!.removeItem(
+        index,
+        (BuildContext context, Animation<double> animation) {
+          return removedItemBuilder(removedItem, context, animation);
+        },
+      );
+    }
+    return removedItem;
+  }
+
+  int get length => _items.length;
+
+  E operator [](int index) => _items[index];
+
+  int indexOf(E item) => _items.indexOf(item);
+}
+
+typedef RemovedItemBuilder<T> = Widget Function(
+    T item, BuildContext context, Animation<double> animation);
+
+class CardItem extends StatelessWidget {
+  const CardItem({
+    Key? key,
+    this.onTap,
+    this.selected = false,
+    required this.animation,
+    required this.item,
+  }) : super(key: key);
+
+  final Animation<double> animation;
+  final VoidCallback? onTap;
+  final FoodsData item;
+  final bool selected;
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle textStyle =
+        Theme.of(context).textTheme.bodyMedium!.copyWith(fontSize: 20);
+    Color containerColor = Color.fromARGB(255, 199, 218, 199);
+    if (selected) {
+      //textStyle = textStyle.copyWith(color: Colors.lightGreenAccent[400]);
+      containerColor = Colors.red;
+    }
+    return Padding(
+        padding: const EdgeInsets.all(2.0),
+        child: SizeTransition(
+          sizeFactor: animation,
+          child: GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onTap: onTap,
+            child: SizedBox(
+              height: 80.0,
+              child: ListTile(
+                tileColor: containerColor,
+                title: Text(item.title, style: textStyle),
                 leading: Container(
                   height: 50,
                   decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(5)),
-                  child: Image.asset(fruits[index].image),
+                      BoxDecoration(borderRadius: BorderRadius.circular(5), ),
+                  child: Image.asset(item.image),
                 ),
-                trailing: InkWell(
-                  onTap: () {
-                    fruits.remove(fruits[index]);
-                    setState(() {
-                      
-                    });
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      height: 30,
-                      width: 30,
-                      decoration: const BoxDecoration(
-                          shape: BoxShape.circle, color: Colors.red),
-                      child: const Icon(
-                        Icons.remove,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              );
-            }),
-            separatorBuilder: (BuildContext context, int index) {
-              return Divider(
-                color: Colors.grey,
-              );
-            },
+              ),
+            ),
           ),
         ));
   }
